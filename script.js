0b319c6527c5bbe546e36bac8ff55cf1375328af
ggd3246 <ggdg81759@gmail.com>
2025-06-13 12:45:28 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,27 @@
+// Открытие модального окна
+document.querySelectorAll('.open-modal').forEach(button => {
+  button.addEventListener('click', () => {
+    const modalId = button.getAttribute('data-modal');
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.style.display = 'block';
+    }
+  });
+});
+
+// Закрытие по нажатию на крестик
+document.querySelectorAll('.close').forEach(closeBtn => {
+  closeBtn.addEventListener('click', () => {
+    const modal = closeBtn.closest('.modal');
+    modal.style.display = 'none';
+  });
+});
+
+// Закрытие при клике вне модального окна
+window.addEventListener('click', (event) => {
+  document.querySelectorAll('.modal').forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
